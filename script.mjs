8d2c8ce3258073781fed4517e8553356a9f7512a
Saba Saeed khan <144582707+saeedsamina@users.noreply.github.com>
2023-12-09 00:51:21 +0500

    greet a user

--- script.mjs
+++ script.mjs
@@ -1,19 +1,7 @@
-//hard coded function
-//function welcomeUser(){
-//          console.log('welcome miss saba saeed khan')
-//}
-//welcomeUser()
-//welcomeUser()
-//welcomeUser()
-//welcomeUser()
-
 function greet_user(username) {
 
     console.log(`welcome Miss ${username}`)
 }
-
-//calling a function
-
 greet_user('Saba saeed khan')
 greet_user('noor e rehman')
 greet_user('manal masood')
